12e6147c0b8fa5b2aaa9a14317833bc01974edaa
Renzo Bautista <renzobautista2@gmail.com>
2016-09-11 21:29:01 -0400

    created initial models

--- portal/models.py
+++ portal/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=255)
+    email_handle = models.CharField(max_length=255)
+
+class Course(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    course_number = models.CharField(max_length=255)
+    course_name = models.CharField(max_length=255)
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='student')
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    ta_courses = models.ManyToManyField(Course)
