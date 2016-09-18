a8d1eb8d9a6ce447c8f02b51e1d05ee6977ae347
Renzo Bautista <renzobautista2@gmail.com>
2016-09-18 03:09:35 -0400

    added ability for professors to add courses and set TAs

--- portal/forms.py
+++ portal/forms.py
@@ -4,6 +4,7 @@ from models import *
 
 class SignupForm(UserCreationForm):
     username = forms.EmailField(label='.edu Email')
+    is_professor = forms.BooleanField(label='I am a professor')
 
     def clean_username(self):
         if (self.cleaned_data['username'] is None):
@@ -21,3 +22,12 @@ class SignupForm(UserCreationForm):
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(max_length=150, widget=forms.PasswordInput)
+
+class CourseForm(forms.ModelForm):
+    tas = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+        label='Teaching Assistants', queryset=Student.objects.all())
+    class Meta:
+        model = Course
+        exclude = ('school', 'professor')
+    def clean_tas(self):
+        return self.cleaned_data['tas']
--- portal/models.py
+++ portal/models.py
@@ -6,11 +6,20 @@ from django.db import models
 # Create your models here.
 
 class School(models.Model):
+    admin_user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                      related_name='admin_school')
     name = models.CharField(max_length=255)
     email_handle = models.CharField(max_length=255)
 
+class Professor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='professor')
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+
 class Course(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     course_number = models.CharField(max_length=255)
     course_name = models.CharField(max_length=255)
 
@@ -18,7 +27,10 @@ class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 related_name='student')
     school = models.ForeignKey(School, on_delete=models.CASCADE)
-    ta_courses = models.ManyToManyField(Course)
+    ta_courses = models.ManyToManyField(Course, related_name='tas')
+
+    def __unicode__(self):
+        return self.user.username
 
 class Stream(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
