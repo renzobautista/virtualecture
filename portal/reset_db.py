8266bae99951cf8cde9c9396f14235470f981dc8
Renzo Bautista <renzobautista2@gmail.com>
2016-09-21 02:52:13 -0400

    merge conflict

--- portal/reset_db.py
+++ portal/reset_db.py
@@ -1,10 +1,6 @@
 from portal.models import *
 
-<<<<<<< HEAD
 cmu_admin = User.objects.create(username='cmuadmin@andrew.cmu.edu', password='carnegie123')
-=======
-cmu_admin = User.objects.create(username='cmu_admin@andrew.cmu.edu', password='carnegie123')
->>>>>>> 9894cc0a4f6817b2a4407b02e9b120f5b90b7062
 cmu_admin.set_password('carnegie123')
 cmu_admin.save()
 cmu = School.objects.create(name="Carnegie Mellon University", email_handle="andrew.cmu.edu", admin_user=cmu_admin)
